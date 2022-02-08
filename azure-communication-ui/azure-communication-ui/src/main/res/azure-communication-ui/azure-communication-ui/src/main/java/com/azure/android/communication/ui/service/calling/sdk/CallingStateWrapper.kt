0963eb825712335111ebd6127560ac0e0e40550a
ShaunaSong <93549644+ShaunaSong@users.noreply.github.com>
2022-02-08 15:19:17 -0400

    Revert "Merge branch 'feature/localization-example' of https://github.com/Azure/communication-ui-library-android into feature/localization-example"
    This reverts commit 83e20de40bb7e1d6383156a1d51c26a86c2c6cbd, reversing
    changes made to d47d6b3c9a289d384bd585d7b91ad0ee4e96f74c.

--- azure-communication-ui/azure-communication-ui/src/main/res/azure-communication-ui/azure-communication-ui/src/main/java/com/azure/android/communication/ui/service/calling/sdk/CallingStateWrapper.kt
+++
@@ -1,5 +0,0 @@
-package com.azure.android.communication.ui.service.calling.sdk
-
-import com.azure.android.communication.calling.CallState
-
-internal data class CallingStateWrapper(val callState: CallState, val callEndReason: Int)
