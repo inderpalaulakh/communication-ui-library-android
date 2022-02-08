2905c723a2633430a000a482e3e94951d0c58ea7
ShaunaSong <93549644+ShaunaSong@users.noreply.github.com>
2022-02-08 14:42:19 -0400

    Revert "[TDBuild-Generated] Update localized files"
    This reverts commit a1d70da7796696d39bd4de9ed57aac6ff24429ca.

--- azure-communication-ui/azure-communication-ui/src/main/res/azure-communication-ui/azure-communication-ui/src/main/res/azure-communication-ui/azure-communication-ui/src/main/java/com/azure/android/communication/ui/error/CallStateError.kt
+++
@@ -1,5 +0,0 @@
-package com.azure.android.communication.ui.error
-
-import com.azure.android.communication.ui.configuration.events.CallCompositeErrorCode
-
-internal data class CallStateError(val callCompositeErrorCode: CallCompositeErrorCode,)
